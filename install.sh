6a2a7321d3ff286f9d6d22d93a69d526e67981ce
xiaoz <xiaoz93@outlook.com>
2025-03-17 13:56:53 +0800

    添加自动化

--- install.sh
+++ install.sh
@@ -1,6 +1,26 @@
-apk add python3
-apk add py3-pip
-mkdir -p  /opt/transmute && cdgit /opt/transmute
-python3 -m venv venv
-source venv/bin/activate
-pip3 install -r requirements.txt
+#!/bin/sh
+
+# 安装依赖
+install_deps(){
+    akp update
+    apk add python3
+    apk add py3-pip
+    mkdir -p  /opt/transmute && cd /opt/transmute
+}
+
+# 安装 Python 依赖
+install_python_deps(){
+    python3 -m venv venv
+    source venv/bin/activate
+    pip3 install -r requirements.txt
+}
+
+
+# 清理缓存，缩小镜像体积
+clean(){
+    rm -rf /var/cache/apk/*
+    rm -rf /root/.cache
+    #rm -rf /opt/transmute/venv
+}
+
+install_deps && install_python_deps && clean
