2b563f9844763373227020771d902e90bbcd5141
xiaoz <xiaoz93@outlook.com>
2025-04-29 11:07:49 +0800

    update

--- app/routers/routers.py
+++ app/routers/routers.py
@@ -1,6 +1,5 @@
 from app.api.translate import Translate
 from app.api.html import Html
-
 from fastapi import APIRouter
 
 # 创建 APIRouter 实例
@@ -8,7 +7,7 @@ router = APIRouter()
 
 
 translate = Translate()
-html =Html()
+html = Html()
 
 router.post("/api/translate")(translate.chat)
 router.get("/api/get/models")(translate.get_models)
